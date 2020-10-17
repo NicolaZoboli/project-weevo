dbbe879fd4be98ecfa1c455ba64af748ab9f4dbd
Nicola Zoboli <64474850+NicolaZoboli@users.noreply.github.com>
2020-10-17 17:13:33 +0200

    Finished non-responsive graphic

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 
+function dropdown() {
+
+  $('.parent-dropdown').hover(function() {
+    $(this).children('.dropdown').show();
+  },
+  function() {
+    $(this).children('.dropdown').hide();
+  });
+}
+
+
 function init() {
 
+  dropdown();
   console.log("Hello World");
 }
 
